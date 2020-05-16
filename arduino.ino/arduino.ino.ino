c1f9b714c9d42077b18033680f1ddd8eaf7221c9
lacie-life <00sao00ios00@gmail.com>
2020-05-16 09:17:09 +0700

    add control by Serial

--- arduino.ino/arduino.ino.ino
+++ arduino.ino/arduino.ino.ino
@@ -1,7 +1,7 @@
 
 // Relay pin is controlled with D8. The active wire is connected to Normally Closed and common
 int relay = 8;
-int state = 0;
+String state = "0";
 
 void setup() {
   // Pin for relay module set as output
@@ -10,12 +10,12 @@ void setup() {
 }
 
 void loop() {
-  if(if (Serial.available() > 0)){
-    state = Serial.read(); // read the incoming byte:
+  if(Serial.available() > 0){
+    state = Serial.readStringUntil('\n'); // read the incoming byte:
 
     Serial.print(" I received:");
     Serial.println(state);
-    if(state == 1){
+    if(state == "1"){
       digitalWrite(relay, HIGH);
       Serial.println("ON");    
     }
