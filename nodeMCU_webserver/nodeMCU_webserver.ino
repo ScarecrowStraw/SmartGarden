899c00b016114f8dc46a7000cb63d333d99fa238
lacie-life <00sao00ios00@gmail.com>
2020-05-20 16:55:09 +0700

    I don't know ...

--- nodeMCU_webserver/nodeMCU_webserver.ino
+++ nodeMCU_webserver/nodeMCU_webserver.ino
@@ -12,8 +12,53 @@ const char* ssid = "TP-Link_C78F";
 const char* password = "0123456@";
 int count = 0;
 
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<h1>WiFi LED on off demo: 1</h1><br>
+Ciclk to turn <a href="ledOn">LED ON</a><br>
+Ciclk to turn <a href="ledOff">LED OFF</a><br>
+<hr>
+<a href="https://circuits4you.com">circuits4you.com</a>
+</center>
+ 
+</body>
+</html>
+)=====";
+
 //Declare a global object variable from the ESP8266WebServer class.
 ESP8266WebServer server(80); //Server on port 80
+
+void handleRoot() {
+ Serial.println("You called root page");
+// String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html",
+  "<meta charset='utf-8'>"
+    "<html>"
+    "<head>"
+    "<title>"
+    "NodeMCU"
+    "</title>"
+    "</head>"
+    "<body>"
+    " Test HTML bla bla !!! "
+    "</body>"
+    "</html>");
+}
+ 
+void handleLEDon() { 
+ Serial.println("LED on page");
+ digitalWrite(LED,LOW); //LED is connected in reverse
+ server.send(200, "text/html", "LED is ON"); //Send ADC value only to client ajax request
+}
+ 
+void handleLEDoff() { 
+ Serial.println("LED off page");
+ digitalWrite(LED,HIGH); //LED off
+ server.send(200, "text/html", "LED is OFF"); //Send ADC value only to client ajax request
+}
 
 //==============================================================
 //                  SETUP
@@ -48,22 +93,9 @@ void setup(void){
     Serial.println("MDNS responder started");
   }
 
-  server.on("/",[]{
-    Serial.println(String("Co ng truy cap") + count++);
-    server.send(200, "text/html",
-    "<meta charset='utf-8'>"
-    "<html>"
-    "<head>"
-    "<title>"
-    "NodeMCU"
-    "</title>"
-    "</head>"
-    "<body>"
-    " Test HTML bla bla !!! "
-    "</body>"
-    "</html>"
-    );
-  });
+  server.on("/", handleRoot);      //Which routine to handle at root location. This is display page
+  server.on("/ledOn", handleLEDon); //as Per  <a href="ledOn">, Subroutine to be called
+  server.on("/ledOff", handleLEDoff);
   server.begin();                  //Start server
   Serial.println("HTTP server started");
 }
