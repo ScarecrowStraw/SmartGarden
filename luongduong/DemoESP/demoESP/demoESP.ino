f1d77e802e94aca214beaa3486f2bc354cc92aa5
luongduong <luongduong29898@gmail.com>
2020-06-06 21:03:49 +0700

    Demover1

--- luongduong/DemoESP/demoESP/demoESP.ino
+++ luongduong/DemoESP/demoESP/demoESP.ino
@@ -9,27 +9,39 @@
 
 #define FIREBASE_HOST "demo1-c11e3.firebaseio.com"
 #define FIREBASE_AUTH ""  
-#define WIFI_SSID "TP-Link_C78F"   
-#define WIFI_PASSWORD "0123456@"
-#define DHTPIN D5
+#define WIFI_SSID "loudou"   
+#define WIFI_PASSWORD "duongpr0"
+#define DHTPIN D3
 #define DHTTYPE DHT21
 
-long TIME_SLEEP_LOOP = 1000*3600;
+long TIME_SLEEP_LOOP = 10000*1;
 long COUNT = 6;
 long TIME_OUT_WATER = 0;
-long TIME_OUT_DEFAULT = TIME_SLEEP_LOOP * COUNT;
+long TIME_OUT_DEFAULT = 100 * 1000;
 long CURRENT_TIME_DEFAULT_SLEEP = 0;
 long TIME_WATER_ = 0;
+long TIME_SENSOR_ = 0;
+long TIME_CHECK_SENSOR = 300 * 1000;
 
 const long utcOffsetInSeconds = 7*3600;
+
 float lux=0;
 int value = 0;
 float h = 0;
 float t = 0;
+
+int run_time = 5;
+
+int red = D6;
+int led = D7;
+int relay1 = D8;
+int relay2 = D4;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
    
 DHT dht(DHTPIN, DHTTYPE);
 BH1750 lightMeter(0x23);
-
+  
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
@@ -58,48 +70,18 @@ byte igrasia[8] = //icon for water droplet
   B01110,
 };
 
-int red = D6;
-int wait_time = 6;
-int run_time = 5;
-int led = D7;
-
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
-void getTime() {
-  Serial.print(daysOfTheWeek[timeClient.getDay()]);
-  Serial.print(", ");
-  Serial.print(timeClient.getHours());
-  Serial.print(":");
-  Serial.print(timeClient.getMinutes());
-  Serial.print(":");
-  Serial.println(timeClient.getSeconds());
-}
-int hour = timeClient.getHours();
-int minutes = timeClient.getMinutes();
-
-void defaut(){
-  if(TIME_WATER_ == TIME_OUT_DEFAULT){
-    tuoi_cay();
-  }
+      //set up
   
-  //delay(1000*3600*wait_time);
-
-}
-
+  
 void setup() {
   ESP.eraseConfig();
   Serial.begin(9600);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
+  if(WiFi.status() == WL_CONNECTED){
+    Serial.print("Connected");
   }
-  
-  Serial.println(WiFi.localIP());
-  Serial.print("connected: ");
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  
+ 
   Wire.begin();
   if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
     Serial.println(F("BH1750 Advanced begin"));
@@ -115,89 +97,163 @@ void setup() {
   pinMode(red, OUTPUT);
   pinMode(A0, INPUT);
 }
-void tuoi_cay(){
-    digitalWrite(red, HIGH);
-    Serial.println("Turn on");
-    delay(1000*run_time);
-    digitalWrite(red, LOW);
-    Serial.println("Turn off");
-}
 
-void set_time_out_water(int cycle){
-  TIME_OUT_WATER = cycle * TIME_SLEEP_LOOP;
-}
+      // defaut 
 
-void mode_1(){
-  timeClient.update();
-    getTime();
-    set_time_out_water(8);
-    
-    
-    Firebase.setFloat("LightMeter: ",lux);
-    Firebase.setInt("DoAmDat_STATUS", value);
-      Firebase.setFloat("Humidity_STATUS", h);
-      Firebase.setFloat("Temperature_STATUS", t);  
-      FirebaseObject object = Firebase.get("/");
-    String path = object.getString("demo");
-   // int time1 = object.getInt("time".child("gio"));
-    Firebase.setString("LEDSTATUS", path);
-   
-   if (Firebase.failed()){
-     Serial.println(Firebase.error());
-     return;
-    }   
+void defaut(){
+  if(TIME_WATER_ == TIME_OUT_DEFAULT){
+      tuoi_cay();
+  }
 }
+      // kiem tra tuoi
 
 void check_tuoi_cay(){
-  if(TIME_WATER_ == TIME_OUT_WATER){
+  if(TIME_WATER_ == TIME_CHECK_SENSOR){
     TIME_WATER_ = 0;
     tuoi_cay();
   }
 }
 
-void doc_cam_bien(){
-    lux = lightMeter.readLightLevel();
-    value = analogRead(A0);
-    h = dht.readHumidity();
-    t = dht.readTemperature();
-   
-    if (isnan(h) || isnan(t)) {
-      Serial.print("Failed to read from DHT sensor!");
-      return;
+int hour = timeClient.getHours();
+int minutes = timeClient.getMinutes();
+
+      // get Time
+
+void getTime() {
+  hour = timeClient.getHours();
+  minutes = timeClient.getMinutes();
+  Serial.print(daysOfTheWeek[timeClient.getDay()]);
+  Serial.print(", ");
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.print(timeClient.getMinutes());
+  Serial.print(":");
+  Serial.println(timeClient.getSeconds());
+}
+
+      // Tuoi cay
+
+void tuoi_cay(){
+    digitalWrite(red, 0);
+    
+    delay(1000*run_time);
+  
+    digitalWrite(red, HIGH);
+    
+}
+
+      // che do online
+
+void mode_1(){
+    
+    timeClient.update();
+    getTime();
+    //set_time_out_water(8);
+    FirebaseObject object = Firebase.get("/");
+    
+    Firebase.setFloat("LightMeter: ",lux);
+    Firebase.setInt("DoAmDat_STATUS", value);
+    Firebase.setFloat("Humidity_STATUS", h);
+    Firebase.setFloat("Temperature_STATUS", t);
+
+      // chon gio tuoi tieu
+    int timegio = object.getInt("gio");
+    int timephut = object.getInt("phut");
+    Serial.print (timegio);
+    Serial.print (timephut);
+    
+    if (timegio == hour  && timephut == minutes){
+        Serial.print("ab");
+        digitalWrite(red, HIGH);
+        delay(1000*5);
     }
-     if ( value < 500 && t < 30){
-        digitalWrite (red, 0);
+    else{
+        digitalWrite(red, LOW);
+    }
+    int timegio2 = object.getInt("gio2");
+    int timephut2 = object.getInt("phut2");
+    
+    if (timegio2 == hour  && timephut2 == minutes){
+        digitalWrite(red, HIGH);
+        delay(1000*5);
+       }
+    else{
+        digitalWrite(red, LOW);
+    }
+   //
+    
+   if (Firebase.failed()){
+     Serial.println(Firebase.error());
+     return;
+    }   
+}
+ 
+    // check cam bien
+    
+void check_sensor(){
+     if(TIME_SENSOR_ == TIME_CHECK_SENSOR){
+     TIME_SENSOR_ = 0;
+     doc_cam_bien();
+    }
+}
+      // kiem tra tuoi cua cam bien
+
+void doc_cam_bien(){
+    
+    if ( value < 500 && t < 30){
+        digitalWrite (red, HIGH);
         delay (1000*5);
+        digitalWrite (red, 0);
     }
     if ( value > 500 && t > 30){
-      digitalWrite (red, 0);
+      digitalWrite (red,HIGH);
       delay(1000*10);
+      digitalWrite (red, 0);
+      
     }
     if ( value > 500 && t < 30){
-      digitalWrite (red, 0);
+      digitalWrite (red, HIGH);
       delay(1000*5);
+      digitalWrite (red, 0);
     }
     if ( hour > 6 && hour < 18){
       if (lux < 500) {
         digitalWrite (led, HIGH);
-      }TIME_WATER_
+      }
       if (lux > 500){
         digitalWrite (led, 0);
       }
     }
 }
+   
+      // Main
 
 void loop() {
-   doc_cam_bien();
-    if (WiFi.status() != WL_CONNECTED) {
-        Serial.print("Set to defaut");
-        TIME_OUT_WATER = TIME_OUT_DEFAULT;
-   }else{
+   lux = lightMeter.readLightLevel();
+   value = analogRead(A0);
+   h = dht.readHumidity();
+   t = dht.readTemperature();
+  
+  if (isnan(h) || isnan(t)) {
+      Serial.print("Failed to read from DHT sensor!");
+  }
+  
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Set to defaut");
+
+    TIME_OUT_WATER = TIME_OUT_DEFAULT;
+   }
+   else{
+      
       mode_1();
    }
+   
+   check_sensor();
    check_tuoi_cay();
    
-    delay(TIME_SLEEP_LOOP);
-    
+   delay(TIME_SLEEP_LOOP);
+   
    TIME_WATER_ += TIME_SLEEP_LOOP;
+   TIME_SENSOR_ += TIME_SLEEP_LOOP;
+
 } 
--- luongduong/demo/app/src/main/java/com/example/demo/Main2Activity.java
+++ luongduong/demo/app/src/main/java/com/example/demo/Main2Activity.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -25,10 +26,12 @@ import java.util.Calendar;
 public class Main2Activity extends AppCompatActivity {
     Button hengio;
     TimePicker timePicker;
+    TimePicker timePicker2;
     TextView textView;
     Calendar calendar;
     DatabaseReference mData;
-    Spinner spinnerdanhsach;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,49 +42,34 @@ public class Main2Activity extends AppCompatActivity {
         hengio = (Button)findViewById(R.id.hengio);
         textView = (TextView)findViewById(R.id.textView);
         timePicker = (TimePicker)findViewById(R.id.Timepicker);
+        timePicker2 = (TimePicker) findViewById(R.id.Timepicker2);
         calendar = Calendar.getInstance();
-        spinnerdanhsach = (Spinner)findViewById(R.id.Spinner);
-
-        final ArrayList<String> arrayVonglap = new ArrayList<String>();
-        arrayVonglap.add("Once time ");
-        arrayVonglap.add("Twice time");
-        arrayVonglap.add("ba ngay moimet lan");
-        arrayVonglap.add("mot tuan mot lan");
-        arrayVonglap.add("mot thang mot lan");
-
-        final ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,arrayVonglap);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-
-        spinnerdanhsach.setAdapter(arrayAdapter);
-        spinnerdanhsach.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(Main2Activity.this, arrayVonglap.get(position), Toast.LENGTH_SHORT);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-
 
         hengio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 calendar.set(Calendar.HOUR_OF_DAY,timePicker.getCurrentHour());
                 calendar.set(Calendar.MINUTE,timePicker.getCurrentMinute());
 
                 int gio = timePicker.getCurrentHour();
                 int phut = timePicker.getCurrentMinute();
+                int gio2 = timePicker2.getCurrentHour();
+                int phut2 = timePicker2.getCurrentMinute();
 
                 String string_gio = String.valueOf(gio);
                 String string_phut = String.valueOf(phut);
+                String string_gio2 = String.valueOf(gio2);
+                String string_phut2 = String.valueOf(phut2);
 
-                mData.child("time").child("gio").setValue(gio);
-                mData.child("time").child("phut").setValue(phut);
 
-                textView.setText("Gio ban dat la: "+ string_gio + " h " + string_phut);
+                mData.child("gio").setValue(gio);
+                mData.child("phut").setValue(phut);
+                mData.child("gio2").setValue(gio2);
+                mData.child("phut2").setValue(phut2);
 
+                textView.setText("Gio ban dat la: "+ string_gio + " h " + string_phut
+                        + " va gio ban dat tiep theo la: " + string_gio2 + " gio " + string_phut2);
             }
         });
 
